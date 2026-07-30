43648a1092d636dc3103cb3b6cc2007bc8c0d6ab
dhlru <neizvestnyjsaz@gmail.com>
2026-07-30 21:59:21 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+// ================================================================
+//  SERVICE WORKER ДЛЯ PUSH-УВЕДОМЛЕНИЙ
+// ================================================================
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('v1').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== 'v1') {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
